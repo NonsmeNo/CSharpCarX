fdb30e8cff15447df845df09d2536b7f782f4a3b
Valeriya <Lera.53rus@yandex.ru>
2024-10-18 18:07:49 +0300

    create list

---
+++ ConsoleApp1/MyList.cs
@@ -0,0 +1,43 @@
+
+//
+public class Mylist
+{
+    private int[] _array = new int[4];
+    private int _count = 0;
+
+    public void Add(int item) 
+    {
+
+    }
+
+    public void Remove(int item) 
+    {
+
+    }
+
+    public void RemoveAt(int index) 
+    {
+        
+    }
+
+    public void RemoveRange(int index1, int index2) 
+    {
+        
+    }
+
+    public void Insert(int index, int item) 
+    {
+
+    }
+
+    public void Clear() 
+    {
+
+    }
+
+    public void ToString(int item) 
+    {
+
+    }
+    
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,59 @@
-﻿internal class Program
+﻿using System.Collections.Specialized;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"bool size {sizeof(bool)}");
+        Console.WriteLine($"byte size {sizeof(byte)} min: {byte.MinValue} max: {byte.MaxValue}");
+        Console.WriteLine($"short size {sizeof(short)} min: {short.MinValue} max: {short.MaxValue}");
+        Console.WriteLine($"int size {sizeof(int)} min: {int.MinValue} max: {int.MaxValue}");
+        Console.WriteLine($"float size {sizeof(float)} min: {float.MinValue} max: {float.MaxValue}");
+        Console.WriteLine($"long size {sizeof(long)} min: {long.MinValue} max: {long.MaxValue}");
 
-        var result = Console.ReadLine();
-        Console.WriteLine(result);
+        string s = "abcd";
+        
+        byte[] b = new byte[8];
+        s+= "e";
+        char c = s[1];
+        s = s.Insert(0, "f");
+
+        Console.WriteLine($"{s} - {b[7]}");
+
+        List<int> list = new List<int>();
+
+        for (int j = 1; j <= 5; j++) 
+            list.Add(j);
+        
+        Console.WriteLine($"list - {string.Join(", ", list)}");
+        list.Remove(2);
+
+        
+
+
+
+        int i = 0;
+        long l = int.MaxValue;
+        i = (int)l;
+
+        object o = i;
+        i = (int)o;
+
+
+         //f(ref o); 
+        Console.WriteLine($"{o}");
+
+
     }
+
+    //ref передаем ссылку
+    //out обязательно присвоить значение переменной, также передаем ссылку
+    //in переменную нельзя изменить
+    
+    public static void f(in int i) 
+    {
+
+    }
+
+
 }

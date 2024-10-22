542d293d4378d854c3c51e21484c247cba2b5214
Lera <Lera.53rus@yandex.ru>
2024-10-22 14:12:33 +0300

    CorrectingMyList

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,34 +10,56 @@ internal class Program
         Console.WriteLine($"int size {sizeof(int)} min: {int.MinValue} max: {int.MaxValue}");
         Console.WriteLine($"float size {sizeof(float)} min: {float.MinValue} max: {float.MaxValue}");
         Console.WriteLine($"long size {sizeof(long)} min: {long.MinValue} max: {long.MaxValue}");
+        Console.WriteLine();
 
-         List<int> list = new List<int>(10);
-        list.Add(10);
-
-         Console.WriteLine($"list - {string.Join(", ", list)}");
-
-         Mylist list2 = new Mylist();
-
-         list2.Add(10);
-         list2.Add(15);
-         list2.Add(20);
-         list2.Add(25);
-         list2.Add(30);
-         list2.Print();
+        //ref передаем ссылку
+        //out обязательно присвоить значение переменной, также передаем ссылку
+        //in переменную нельзя изменить
 
 
+        //работа с MyList
+        MyList list = new MyList();
 
+        for (int i = 0; i < 10; i++)
+            list.Add(i*5);
 
-    }
+        Console.WriteLine($"Количество элементов в списке: {list.Count}");
+        Console.WriteLine($"Список: {list.ToString()}");
+        Console.WriteLine();
 
-    //ref передаем ссылку
-    //out обязательно присвоить значение переменной, также передаем ссылку
-    //in переменную нельзя изменить
-    
-    public static void f(int i) 
-    {
+        Console.WriteLine("Удаление элемента со значением 30");
+        list.Remove(30);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine();
 
-    }
+        Console.WriteLine("Удаление элемента со значением 100");
+        list.Remove(100);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine();
 
+        Console.WriteLine("Удаление элемента с индексом 2");
+        list.RemoveAt(2);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine();
 
+        Console.WriteLine("Вставка элемента 55 с индексом 3");
+        list.Insert(3, 55);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine();
+
+        Console.WriteLine("Вставка элемента 55 с индексом 10");
+        list.Insert(10, 55);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine();
+
+        Console.WriteLine("Удаление элементов со 2 по 5 индекс");
+        list.RemoveRange(2, 5);
+        Console.WriteLine(list.ToString());
+        Console.WriteLine();
+
+        Console.WriteLine("Очищение списка");
+        list.Clear();
+        Console.WriteLine($"Список: {list.ToString()}");
+        Console.WriteLine();
+    }   
 }
